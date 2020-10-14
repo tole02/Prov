e5bd3c8f2661133a6e50492800ac3e45baa5ad06
Tobias <tobias.lehman@elev.ga.ntig.se>
2020-10-14 10:16:22 +0200

    Prov

---
+++ Prov/Book.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace Prov
+{
+    public class Book
+    {
+        public int price = 0;
+
+        private string name = "";
+
+        private int rarity = 0;
+
+        private string category = "";
+
+        private int actualValue = 0;
+
+        private bool cursed = false;
+
+        private Random generator = new Random();
+
+        public Book(){
+
+        }
+
+        public void PrintInfo(){
+
+        }
+
+        public int Evaluate(){
+            return;
+        }
+
+        public string GetCategory(){
+
+        }
+
+        public string GetName(){
+
+        }
+
+        public bool IsCursed(){
+            
+        }
+
+
+
+
+
+    }
+}
